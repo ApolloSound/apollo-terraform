01826e26681c72600861b5c9ebba4b2ff0c036c2
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2022-09-22 17:15:37 +0200

    Created ecs-role files

---
+++ modules/ecs/role/variable.tf
@@ -0,0 +1,10 @@
+variable "application" {
+  type        = string
+  description = "The name of the application"
+}
+
+variable "environment" {
+  type        = string
+  description = "The environment of the application"
+}
+
