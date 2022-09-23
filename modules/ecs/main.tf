2c2937eedba200318d68fe1f8580f38aede8ccb6
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2022-09-23 10:29:30 +0200

    Created and attached ECS role

--- modules/ecs/main.tf
+++ modules/ecs/main.tf
@@ -1,3 +1,9 @@
+module "ecs-role" {
+  source      = "./role"
+  application = var.application
+  environment = var.environment
+}
+
 resource "aws_ecs_cluster" "ecs_cluster" {
   name = "${var.application}-${var.environment}-ecs-cluster"
 }
@@ -14,20 +20,22 @@ resource "aws_ecs_task_definition" "task_definition" {
   network_mode             = "awsvpc"
   cpu                      = 512
   memory                   = 1024
-  container_definitions    = jsonencode([
-    {
-      name         = "first"
-      image        = var.image_url
-      cpu          = 10
-      memory       = 512
-      essential    = true
-      portMappings = [
-        {
-          containerPort = 8080
-          hostPort      = 8080
-        }
-      ]
-    }
-  ])
-  family = "service"
+  execution_role_arn       = module.ecs-role.ecs_task_execution_role_arn
+  container_definitions    = jsonencode(
+[
+{
+name = "first"
+image = var.image_url
+cpu = 10
+memory = 512
+essential = true
+portMappings = [
+{
+containerPort = 8080
+hostPort = 8080
+}
+]
+}
+])
+family = "service"
 }
